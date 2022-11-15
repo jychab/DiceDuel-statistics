060e24a59133bf7ad8afe6b24e2a5819a3fa3e83
junyangchan <kardxsg@gmail.com>
2022-11-16 01:13:40 +0800

    add no cache to request header

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -81,7 +81,13 @@ export async function getData(): Promise<[RawData]> {
 async function getEnrichedTransactions(url: string): Promise<[RawData] | any> {
   let transactions: [RawData];
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      headers: {
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
+    });
     transactions = response.data;
   } catch (err) {
     console.log(err);
